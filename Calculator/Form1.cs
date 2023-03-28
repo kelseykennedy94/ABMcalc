fdb5bf9ccbdb15a69b72ed06c740767b84287dbc
kelseykennedy94 <pken1664@gmail.com>
2023-03-28 17:55:46 -0600

    kelsey code added

--- Calculator/Form1.cs
+++ Calculator/Form1.cs
@@ -192,6 +192,21 @@ namespace Calculator
             return num1 / num2;
         }
 
+        public double add(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double subtract(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double multiply(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+
 
     }
 }
